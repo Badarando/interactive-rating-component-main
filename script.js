68701625c5d96d38c56acc80e2a42339e4a0b1d5
Marcos Paulo Badaró <marcospaulobadaro@gmail.com>
2022-07-10 17:56:27 -0300

    continuando com o projeto

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 function redirecionar() {
     window.location.href = './confirmation.html'
 }
+
+// const teste = document.querySelectorAll('.number');
+
+// function active(index, array) {
+//     teste.
+
+//     console.log(teste);
+// }
+// function active(item, indice, array){
+//     item = indice++;
+//     const numeros = document.querySelectorAll(".number");
+//     return numeros[indice].classList.add("active");
+// }
+// numeros.forEach(active());
